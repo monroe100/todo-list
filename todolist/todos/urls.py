2407d03c5f46f144b608a2abebed939277000d78
michelle <michelle1hunnid@gmail.com>
2019-06-01 16:53:42 +0300

    create todo details view

--- todolist/todos/urls.py
+++ todolist/todos/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-
-
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^details/(?P<id>\w{0,50})/$', view.details)
     
 ]
