d8d3150de95098e140e69e2cdcf0f70e89707536
michelle <michelle1hunnid@gmail.com>
2019-06-01 16:02:46 +0300

    create html file

--- todolist/todos/views.py
+++ todolist/todos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Michelle')
+    todos = Todo.objects.all()[:10]
+
+    context = {
+        'todos':todos
+    }
+
+    return render(request, 'index.html', context)
