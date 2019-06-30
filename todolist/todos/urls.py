c8c6561ede28d9b981a577b04278a53096bf3ac8
michelle <michelle1hunnid@gmail.com>
2019-06-30 15:58:26 +0300

    style the app

--- todolist/todos/urls.py
+++ todolist/todos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^details/(?P<id>\w{0,50})/$', view.details)
+    url(r'^details/(?P<id>\w{0,50})/$', views.details),
+    url(r'^add', views.add, name='add')
     
 ]
